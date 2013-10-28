b61b53812a0589080c1517dd13f54fba40dc0cb4
Brian Clements <brian@clementscode.com>
2013-10-27 19:22:21 -0500

    added growth, problem with get()

--- src/com/clementscode/ds/Vector.java
+++ src/com/clementscode/ds/Vector.java
@@ -2,6 +2,8 @@ package com.clementscode.ds;
 
 public class Vector<T> {
 
+	private static final int EXP_THRESH = 1024;
+
 	private Object[] data;
 	private int pointer = 0;
 
@@ -10,9 +12,28 @@ public class Vector<T> {
 	}
 
 	public void add(T item) {
+		if (data.length == 0) {
+			data = new Object[8];
+		}
+		if (pointer >= data.length) {
+			grow();
+		}
 		data[pointer++] = item;
 	}
 
+	private void grow() {
+		int growBy = data.length;
+		if (data.length > EXP_THRESH) {
+			growBy /= 2;
+		}
+		Object[] tmp = new Object[data.length + growBy];
+		for (int i = 0; i < this.pointer; ++i) {
+			tmp[i] = this.data[i];
+		}
+		this.data = tmp;
+	}
+
+	@SuppressWarnings("unchecked")
 	public T get(int index) {
 		return (T) data[pointer - 1];
 	}
--- src/com/clementscode/ds/VectorTest.java
+++ src/com/clementscode/ds/VectorTest.java
@@ -8,10 +8,14 @@ public class VectorTest {
 
 	@Test
 	public void testAdd() {
-		Vector<Integer> v = new Vector<Integer>(8);
+		Vector<Integer> v = new Vector<Integer>(0);
 		v.add(41);
 		Assert.assertEquals(1, v.size());
 		Integer res = v.get(0);
+		Assert.assertEquals((Integer) 41, res);
+		v.add(42);
+		Assert.assertEquals(2, v.size());
+		res = v.get(0);
 		Assert.assertEquals((Integer) 41, res);
 	}
 
