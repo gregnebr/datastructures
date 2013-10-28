654cd8167bd654a62f3dbafb68b44eee03e444c5
Brian Clements <brian@clementscode.com>
2013-10-27 19:34:04 -0500

    added collection interface

--- src/com/clementscode/ds/Vector.java
+++ src/com/clementscode/ds/Vector.java
@@ -1,6 +1,9 @@
 package com.clementscode.ds;
 
-public class Vector<T> {
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Vector<T> implements Collection<T> {
 
 	private static final int EXP_THRESH = 1024;
 
@@ -11,7 +14,8 @@ public class Vector<T> {
 		data = new Object[initialSize];
 	}
 
-	public void add(T item) {
+	@Override
+	public boolean add(T item) {
 		if (data.length == 0) {
 			data = new Object[8];
 		}
@@ -19,6 +23,7 @@ public class Vector<T> {
 			grow();
 		}
 		data[pointer++] = item;
+		return true;
 	}
 
 	private void grow() {
@@ -40,6 +45,72 @@ public class Vector<T> {
 
 	public int size() {
 		return pointer;
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends T> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
