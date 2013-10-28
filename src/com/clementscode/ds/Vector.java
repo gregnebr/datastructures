bf6d2be652256a72591f1333b80314489043d823
Greg Clements <gregnebriowa@gmail.com>
2013-10-27 19:05:42 -0500

    Works

--- src/com/clementscode/ds/Vector.java
+++ src/com/clementscode/ds/Vector.java
@@ -3,21 +3,22 @@ package com.clementscode.ds;
 public class Vector<T> {
 
 	private Object[] data;
+	private int pointer = 0;
 
 	public Vector(int initialSize) {
 		data = new Object[initialSize];
 	}
 
 	public void add(T item) {
-
+		data[pointer++] = item;
 	}
 
 	public T get(int index) {
-		return null;
+		return (T) data[pointer - 1];
 	}
 
 	public int size() {
-		return 0;
+		return pointer;
 	}
 
 }
